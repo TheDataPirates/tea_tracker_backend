1d9258fb0a1df54bd95fce12d31e5538ad8b5935
DamithaNeranjan <41992701+DamithaNeranjan@users.noreply.github.com>
2020-11-08 20:46:36 +0530

    Trough loading backend done.

--- controllers/loft.js
+++ controllers/loft.js
@@ -1,4 +1,5 @@
 const trough_process = require("../models/trough_process");
+const Loaded_Bulk_Box = require("../models/loaded_bulk_box");
 const { Op } = require("sequelize");
 
 
@@ -129,6 +130,53 @@ exports.createFinishing = async (req, res, next) => {
       ProcessProcessName: proc_name,
     });
     console.log("finishing saved");
+    res.status(200).json({
+      starting: "saved",
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.getLoadings = async (req, res, next) => {
+  try {
+    const allLoadings = await Loaded_Bulk_Box.findAll(
+    //   {
+    //   where: { date: getDate() },
+    // }
+    );
+    res.status(200).json({
+      loadings: allLoadings,
+    });
+  } catch (error) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.createLoading = async (req, res, next) => {
+  const id = req.body.id;
+  const trough_no = req.body.troughNumber;
+  const date = req.body.date;
+  const box_no = req.body.boxNumber;
+  const leaf_grade = req.body.gradeOfGL;
+  const net_weight = req.body.netWeight;
+  try {
+    await Loaded_Bulk_Box.create({
+      load_bulk_box_id: id,
+      date: date,
+      leaf_grade: leaf_grade,
+      net_weight: net_weight,
+      box_number: box_no,
+      trough_number: trough_no,
+    });
+    // console.log(box_no);
+    console.log("Loading saved");
     res.status(200).json({
       starting: "saved",
     });
