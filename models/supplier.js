cd7ee7e4450f77eaef8fc6044b25ba38b754979c
deveen <deveenrath@gmail.com>
2020-11-09 21:14:31 +0530

    before merge with damiya

--- models/supplier.js
+++ models/supplier.js
@@ -1,13 +1,10 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../database/db");
 
-
-module.exports= sequelize.define('Supplier', {
-    supplier_id: {
-        type: Sequelize.STRING,
-        primaryKey: true,
-        
-    },
-    fname: Sequelize.STRING,
-    
+module.exports = sequelize.define("Supplier", {
+  supplier_id: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  fname: Sequelize.STRING,
 });
