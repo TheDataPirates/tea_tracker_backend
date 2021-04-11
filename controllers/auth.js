9ea1adb7ce2b8720b5356d603a73cc67d94aaa1e
DamithaNeranjan <41992701+DamithaNeranjan@users.noreply.github.com>
2021-04-11 16:09:47 +0530

    Login method changed to have email and password and ssl problem is solved by adding tsl rejectUnauthorized to false in email.send.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const {Op} = require("sequelize");
+const { Op } = require("sequelize");
 const User = require("../models/user");
 const templates = require("../email/email.templates");
 const sendEmail = require("../email/email.send");
@@ -88,12 +88,12 @@ exports.login = async (req, res, next) => {
 };
 
 exports.loginWeb = async (req, res, next) => {
-    const userid = req.body.user_id;
+    const email = req.body.email;
     const password = req.body.password;
     let loadedUser;
     const user = await User.findOne({
         where: {
-            user_id: userid, [Op.or]: [
+            email: email, [Op.or]: [
                 { user_type: "Manager" },
                 { user_type: "Admin" },
             ],
@@ -131,35 +131,51 @@ exports.loginWeb = async (req, res, next) => {
 };
 
 //forgotPassowrd
-exports.forgotPassword = function (req, res,next) {
+exports.forgotPassword = function (req, res, next) {
 
     const email = req.params.email;
     // console.log(email);
-    User.findOne({where: {email: email}})
+    User.findOne({
+        where: {
+            email: email, [Op.or]: [
+                { user_type: "Manager" },
+                { user_type: "Admin" },
+            ],
+        }
+    })
         .then((user) => {
             if (!user) {
-                err = "User not found";
-                return res.status(404).json(err);
+                err = "User Not Found !!!";
+                console.log("No User");
+                // console.log(user.email);
+                const error = new Error(err);
+                error.statusCode = 404;
+                next(error);
+
             } else if (user) {
                 sendEmail(user.email, templates.resetPassword(user.user_id));
+                console.log("Has User");
                 console.log(user.email);
-                console.log(user.user_id);
-                console.log(msgs.forgotPassword);
+                // console.log(user.email);
+                // console.log(user.user_id);
+                // console.log(msgs.forgotPassword);
                 return res.status(200).json(msgs.forgotPassword);
             } else {
+                console.log("Else User");
+                // console.log(user.email);
                 err = "Cannot send email to this address";
                 return res.status(404).json(err);
             }
         }).catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 }
 
 //reset password
-exports.resetPassword = (req, res,next) => {
+exports.resetPassword = (req, res, next) => {
     // var validPasswordRegex = RegExp(
     //     /^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[^a-zA-Z0-9])(?!.*\s).{6,20}$/
     // );
@@ -173,69 +189,69 @@ exports.resetPassword = (req, res,next) => {
     //     passerr = "Passwords must match!";
     //     return res.status(400).json(passerr);
     // } else {
-        User.findOne({where: {email}})
-            .then(user => {
-                // A user with that id does not exist in the DB. Perhaps some tricky
-                // user tried to go to a different url than the one provided in the
-                // confirmation email.
-                if (!user) {
-                    res.json({msg: msgs.couldNotFind});
-                }
-                    // The user exists but has not been confirmed. We need to confirm this
-                // user and let them know their email address has been confirmed.
-                else if (user) {
-                    bcrypt.genSalt(8, function (err, salt) {
-                        bcrypt.hash(newpassword, salt, (err, hash) => {
-                            if (err) throw err;
-                            User.password = hash;
-                            User.update(
-                                {
-                                    password: hash,
-                                },
-                                {where: {email}}
-                            ).then((user) => {
-                                if (!user) {
-                                    err = "User not found";
-                                    return res.status(404).json(err);
-                                } else {
-                                    console.log(msgs.resetPassword)
-                                    res.json({
-                                        success: true,
-                                        msg: msgs.resetPassword,
-                                    });
-                                }
-                            }).catch(err => {
-                                if (!err.statusCode) {
-                                    err.statusCode = 500;
-                                }
-                                next(err);
-                            });
-                        })
-                    });
-                    // let hashedpw = null;
-                    // await bcrypt.genSalt(8, function (err, salt) {
-                    //     console.log(salt);
-                    //     bcrypt.hash(password, salt, function (err, hash) {
-                    //         if (err) {
-                    //             next(err);
-                    //         }
-                    //         hashedpw = hash;
-                    //         console.log(hashedpw);
-                    //         // Store hash in your password DB.
-                    //     });
-                    // });
-                }
-                // The user has already confirmed this email address.
-                else {
-                    res.json({msg: msgs.errorPassword})
-                }
-            })
-            .catch(err => {
-                if (!err.statusCode) {
-                    err.statusCode = 500;
-                }
-                next(err);
-            });
+    User.findOne({ where: { email } })
+        .then(user => {
+            // A user with that id does not exist in the DB. Perhaps some tricky
+            // user tried to go to a different url than the one provided in the
+            // confirmation email.
+            if (!user) {
+                res.json({ msg: msgs.couldNotFind });
+            }
+            // The user exists but has not been confirmed. We need to confirm this
+            // user and let them know their email address has been confirmed.
+            else if (user) {
+                bcrypt.genSalt(8, function (err, salt) {
+                    bcrypt.hash(newpassword, salt, (err, hash) => {
+                        if (err) throw err;
+                        User.password = hash;
+                        User.update(
+                            {
+                                password: hash,
+                            },
+                            { where: { email } }
+                        ).then((user) => {
+                            if (!user) {
+                                err = "User not found";
+                                return res.status(404).json(err);
+                            } else {
+                                console.log(msgs.resetPassword)
+                                res.json({
+                                    success: true,
+                                    msg: msgs.resetPassword,
+                                });
+                            }
+                        }).catch(err => {
+                            if (!err.statusCode) {
+                                err.statusCode = 500;
+                            }
+                            next(err);
+                        });
+                    })
+                });
+                // let hashedpw = null;
+                // await bcrypt.genSalt(8, function (err, salt) {
+                //     console.log(salt);
+                //     bcrypt.hash(password, salt, function (err, hash) {
+                //         if (err) {
+                //             next(err);
+                //         }
+                //         hashedpw = hash;
+                //         console.log(hashedpw);
+                //         // Store hash in your password DB.
+                //     });
+                // });
+            }
+            // The user has already confirmed this email address.
+            else {
+                res.json({ msg: msgs.errorPassword })
+            }
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
     // }
 }
 exports.getUsers = async (req, res, next) => {
@@ -310,7 +326,7 @@ exports.updateUser = async (req, res, next) => {
 exports.deleteUser = async (req, res, next) => {
     const user_id = req.params.userId;
 
-    let user = await User.destroy({where: {user_id}}).catch((err) => {
+    let user = await User.destroy({ where: { user_id } }).catch((err) => {
         if (!err.statusCode) {
             err.statusCode = 500;
         }
@@ -318,7 +334,7 @@ exports.deleteUser = async (req, res, next) => {
     });
     if (!user) {
         console.log("user not found");
-        res.status(500).json({message: "user not found"});
+        res.status(500).json({ message: "user not found" });
     } else {
         res.status(200).json({
             user: "Deleted",
